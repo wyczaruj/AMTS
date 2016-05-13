e8d88bbb3b4b2b994e1da35c2533e82c2946c4a1
hgvjhgjhg <wyczaruj.smoka@hotmail.com>
2016-05-13 12:25:32 +0200

    wyniki

--- AMTS/WynikiForm.Designer.cs
+++ AMTS/WynikiForm.Designer.cs
@@ -38,6 +38,7 @@
             this.edit = new System.Windows.Forms.Button();
             this.discard = new System.Windows.Forms.Button();
             this.save = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.aMTSDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.wynikBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -74,9 +75,9 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(604, 139);
+            this.comboBox1.Location = new System.Drawing.Point(627, 139);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(49, 24);
+            this.comboBox1.Size = new System.Drawing.Size(75, 24);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
             // 
@@ -91,37 +92,51 @@
             // 
             // edit
             // 
-            this.edit.Location = new System.Drawing.Point(494, 169);
+            this.edit.Location = new System.Drawing.Point(522, 169);
             this.edit.Name = "edit";
             this.edit.Size = new System.Drawing.Size(159, 33);
             this.edit.TabIndex = 3;
             this.edit.Text = "Edytuj";
             this.edit.UseVisualStyleBackColor = true;
+            this.edit.Click += new System.EventHandler(this.edit_Click);
             // 
             // discard
             // 
             this.discard.Location = new System.Drawing.Point(494, 208);
             this.discard.Name = "discard";
-            this.discard.Size = new System.Drawing.Size(75, 33);
+            this.discard.Size = new System.Drawing.Size(86, 33);
             this.discard.TabIndex = 4;
             this.discard.Text = "Anuluj";
             this.discard.UseVisualStyleBackColor = true;
+            this.discard.Click += new System.EventHandler(this.discard_Click);
             // 
             // save
             // 
-            this.save.Location = new System.Drawing.Point(578, 208);
+            this.save.Location = new System.Drawing.Point(619, 208);
             this.save.Name = "save";
-            this.save.Size = new System.Drawing.Size(75, 33);
+            this.save.Size = new System.Drawing.Size(83, 33);
             this.save.TabIndex = 5;
             this.save.Text = "Zapisz";
             this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(494, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Zmiany nie zostały wprowadzone";
             // 
             // WynikiForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(665, 400);
+            this.ClientSize = new System.Drawing.Size(714, 400);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.save);
             this.Controls.Add(this.discard);
             this.Controls.Add(this.edit);
@@ -150,5 +165,6 @@
         private System.Windows.Forms.Button edit;
         private System.Windows.Forms.Button discard;
         private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Label label2;
     }
 }
--- AMTS/WynikiForm.cs
+++ AMTS/WynikiForm.cs
@@ -18,6 +18,7 @@ namespace AMTS
         DataSet dataSet;
         SqlDataAdapter dataAd;
         SqlDataAdapter data;
+        List<string> druzyny;
 
         public WynikiForm()
         {
@@ -27,6 +28,8 @@ namespace AMTS
         public WynikiForm(SqlConnection connection, MainForm mainForm, bool admin)
         {
             InitializeComponent();
+            druzyny = new List<string>();
+            label2.Visible = false;
             this.connection = connection;
             this.mainForm = mainForm;
             dataSet = new DataSet();
@@ -38,9 +41,9 @@ namespace AMTS
             if (!admin)
             {
                 edit.Visible = false;
-                discard.Visible = false;
-                save.Visible = false;
             }
+            discard.Visible = false;
+            save.Visible = false;
             comboBox1.Items.Add(" ");
             SqlCommand com = new SqlCommand("select distinct R from wynik", connection);
             SqlDataReader reader = com.ExecuteReader();
@@ -52,11 +55,22 @@ namespace AMTS
                 }
             }
             reader.Close();
+            SqlCommand co = new SqlCommand("SELECT * FROM DRUZYNY", connection);
+
+            SqlDataReader reade = co.ExecuteReader();
+
+            if (reade.HasRows)
+            {
+                while (reade.Read())
+                {
+                    druzyny.Add(reade.GetString(0));
+                }
+            }
+            reade.Close();
         }
 
         private void WynikiForm_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'aMTSDataSet.wynik' table. You can move, or remove it, as needed.
             this.wynikTableAdapter.Fill(this.aMTSDataSet.wynik);
 
         }
@@ -69,7 +83,7 @@ namespace AMTS
         private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
         {
             dataSet.Clear();
-            if(comboBox1.SelectedItem.ToString().Equals(" "))
+            if (comboBox1.SelectedItem.ToString().Equals(" "))
             {
                 dataAd.Fill(dataSet, "WYNIKI");
                 dataGridView1.DataSource = dataSet.Tables["WYNIKI"];
@@ -81,6 +95,130 @@ namespace AMTS
                 data.Fill(dataSet, "WYNIKI");
                 dataGridView1.DataSource = dataSet.Tables["WYNIKI"];
             }
+        }
+
+        private void edit_Click(object sender, EventArgs e)
+        {
+            discard.Visible = true;
+            save.Visible = true;
+            label2.Visible = false;
+            dataGridView1.ReadOnly = false;
+            dataGridView1.Columns[0].ReadOnly = true;
+            dataGridView1.Columns[1].ReadOnly = true;
+            dataGridView1.Columns[2].ReadOnly = true;
+        }
+
+        private void discard_Click(object sender, EventArgs e)
+        {
+            dataSet.Clear();
+            if (comboBox1.SelectedIndex > 0)
+            {
+                data = new SqlDataAdapter("select * from dbo.wynikirundy(" + comboBox1.SelectedItem + ")", connection);
+                SqlCommandBuilder com = new SqlCommandBuilder(data);
+                data.Fill(dataSet, "WYNIKI");
+                dataGridView1.DataSource = dataSet.Tables["WYNIKI"];
+            }
+            else
+            {
+                dataAd.Fill(dataSet, "WYNIKI");
+                dataGridView1.DataSource = dataSet.Tables["WYNIKI"];
+            }
+            discard.Visible = false;
+            save.Visible = false;
+            dataGridView1.ReadOnly = true;
+            label2.Visible = false;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            int first, second, third, fourth;
+            bool noError = true;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (!int.TryParse(row.Cells[3].Value.ToString(), out first))
+                {
+                    label2.Visible = true;
+                    noError = false;
+                }
+                if (!int.TryParse(row.Cells[4].Value.ToString(), out second))
+                {
+                    label2.Visible = true;
+                    noError = false;
+                }
+                if (!int.TryParse(row.Cells[5].Value.ToString(), out third))
+                {
+                    label2.Visible = true;
+                    noError = false;
+                }
+                if (!int.TryParse(row.Cells[6].Value.ToString(), out fourth))
+                {
+                    label2.Visible = true;
+                    noError = false;
+                }
+                if (noError)
+                {
+                    if (first != 0 || second != 0 || third != 0 || fourth != 0)
+                    {
+                        if (first + second != 3)
+                        {
+                            noError = false;
+                            label2.Visible = true;
+                        }
+                        else
+                        {
+                            if (first > second)
+                            {
+                                if (third <= fourth)
+                                {
+                                    noError = false;
+                                    label2.Visible = true;
+                                }
+                            }
+                            else
+                            {
+                                if (third >= fourth)
+                                {
+                                    noError = false;
+                                    label2.Visible = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (noError)
+            {
+                int r;
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    int.TryParse(row.Cells[3].Value.ToString(), out first);
+                    int.TryParse(row.Cells[4].Value.ToString(), out second);
+                    int.TryParse(row.Cells[5].Value.ToString(), out third);
+                    int.TryParse(row.Cells[6].Value.ToString(), out fourth);
+                    int.TryParse(row.Cells[0].Value.ToString(), out r);
+                    SqlCommand com = new SqlCommand("update wynik set D1=" + first + ", D2=" + second + ", M1 =" + third + ", M2 =" + fourth + " where Druzyna = '" + row.Cells[1].Value.ToString() + "' and Przeciwnik = '" + row.Cells[2].Value.ToString() + "' and R=" + r, connection);
+                    com.ExecuteNonQuery(); 
+                }
+            }
+            else
+            {
+                dataSet.Clear();
+                if (comboBox1.SelectedIndex > 0)
+                {
+                    data = new SqlDataAdapter("select * from dbo.wynikirundy(" + comboBox1.SelectedItem + ")", connection);
+                    SqlCommandBuilder com = new SqlCommandBuilder(data);
+                    data.Fill(dataSet, "WYNIKI");
+                    dataGridView1.DataSource = dataSet.Tables["WYNIKI"];
+                }
+                else
+                {
+                    dataAd.Fill(dataSet, "WYNIKI");
+                    dataGridView1.DataSource = dataSet.Tables["WYNIKI"];
+                }
+            }
+            discard.Visible = false;
+            save.Visible = false;
+            dataGridView1.ReadOnly = true;
         }
     }
 }
