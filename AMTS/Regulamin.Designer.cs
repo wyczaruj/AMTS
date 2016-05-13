c3a7f0a3409eeb75a671a943e55515d5a4612d1a
Patryk <patrykvan@gmail.com>
2016-05-13 20:35:08 +0200

    Revert "Dodanie PdfiumViewer"

--- AMTS/Regulamin.Designer.cs
+++ AMTS/Regulamin.Designer.cs
@@ -28,34 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pdfViewer = new PdfiumViewer.PdfViewer();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Regulamin));
+            this.Zamknij = new System.Windows.Forms.Button();
+            this.wczytaj = new System.Windows.Forms.Button();
+            this.RegulaminAxAcroPDF = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(this.RegulaminAxAcroPDF)).BeginInit();
             this.SuspendLayout();
             // 
-            // pdfViewer
+            // Zamknij
             // 
-            this.pdfViewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pdfViewer.Location = new System.Drawing.Point(0, 0);
-            this.pdfViewer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.pdfViewer.Name = "pdfViewer";
-            this.pdfViewer.Size = new System.Drawing.Size(1294, 709);
-            this.pdfViewer.TabIndex = 5;
-            this.pdfViewer.ZoomMode = PdfiumViewer.PdfViewerZoomMode.FitWidth;
+            this.Zamknij.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Zamknij.Location = new System.Drawing.Point(1167, 664);
+            this.Zamknij.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Zamknij.Name = "Zamknij";
+            this.Zamknij.Size = new System.Drawing.Size(103, 32);
+            this.Zamknij.TabIndex = 2;
+            this.Zamknij.Text = "Zamknij";
+            this.Zamknij.UseVisualStyleBackColor = true;
+            this.Zamknij.Click += new System.EventHandler(this.Zamknij_Click);
+            // 
+            // wczytaj
+            // 
+            this.wczytaj.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.wczytaj.Location = new System.Drawing.Point(1167, 13);
+            this.wczytaj.Margin = new System.Windows.Forms.Padding(4);
+            this.wczytaj.Name = "wczytaj";
+            this.wczytaj.Size = new System.Drawing.Size(100, 28);
+            this.wczytaj.TabIndex = 4;
+            this.wczytaj.Text = "Wczytaj plik";
+            this.wczytaj.UseVisualStyleBackColor = true;
+            this.wczytaj.Visible = false;
+            // 
+            // RegulaminAxAcroPDF
+            // 
+            this.RegulaminAxAcroPDF.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RegulaminAxAcroPDF.Enabled = true;
+            this.RegulaminAxAcroPDF.Location = new System.Drawing.Point(0, 0);
+            this.RegulaminAxAcroPDF.Name = "RegulaminAxAcroPDF";
+            this.RegulaminAxAcroPDF.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("RegulaminAxAcroPDF.OcxState")));
+            this.RegulaminAxAcroPDF.Size = new System.Drawing.Size(1294, 709);
+            this.RegulaminAxAcroPDF.TabIndex = 5;
             // 
             // Regulamin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1294, 709);
-            this.Controls.Add(this.pdfViewer);
+            this.Controls.Add(this.wczytaj);
+            this.Controls.Add(this.Zamknij);
+            this.Controls.Add(this.RegulaminAxAcroPDF);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Regulamin";
             this.Text = "Regulamin";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Regulamin_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.RegulaminAxAcroPDF)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private PdfiumViewer.PdfViewer pdfViewer;
+
+        private System.Windows.Forms.Button Zamknij;
+        private System.Windows.Forms.Button wczytaj;
+        private AxAcroPDFLib.AxAcroPDF RegulaminAxAcroPDF;
     }
 }
--- AMTS/Regulamin.cs
+++ AMTS/Regulamin.cs
@@ -1,11 +1,21 @@
-﻿using PdfiumViewer;
-using System;
+﻿using System;
 using System.Windows.Forms;
 
 namespace AMTS
 {
     public partial class Regulamin:AbstractForm
     {
+
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        }
 
         AbstractForm form;
         public Regulamin(bool AdminLogged, AbstractForm form)
@@ -14,16 +24,13 @@ namespace AMTS
             this.form = form;
             if(AdminLogged)
             {
-           //     wczytaj.Visible = true;
+                wczytaj.Visible = true;
             }
             string fileName = "Regulamin.pdf";
             string path = System.IO.Path.Combine(Environment.CurrentDirectory, @"", fileName);
             string newPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(path, @"..\..\..\Data\" + fileName));
-            if (pdfViewer.Document != null)
-            {
-                pdfViewer.Document.Dispose();
-            }
-            pdfViewer.Document = PdfDocument.Load(newPath);
+            var acro = (AcroPDFLib.IAcroAXDocShim)RegulaminAxAcroPDF.GetOcx();
+            acro.src = newPath;
         }
 
         private void Zamknij_Click(object sender, EventArgs e)
