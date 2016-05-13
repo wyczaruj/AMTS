fe9fa8aa9af57c8eb67264814e658345a85a0174
hgvjhgjhg <wyczaruj.smoka@hotmail.com>
2016-05-13 11:14:20 +0200

    wyniki według rundy

--- AMTS/WynikiForm.Designer.cs
+++ AMTS/WynikiForm.Designer.cs
@@ -33,6 +33,11 @@
             this.wynikBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.wynikTableAdapter = new AMTS.AMTSDataSetTableAdapters.wynikTableAdapter();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.edit = new System.Windows.Forms.Button();
+            this.discard = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.aMTSDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.wynikBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -63,23 +68,75 @@
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(488, 488);
+            this.dataGridView1.Size = new System.Drawing.Size(488, 402);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(604, 139);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(49, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(494, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Wybierz rundę";
+            // 
+            // edit
+            // 
+            this.edit.Location = new System.Drawing.Point(494, 169);
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(159, 33);
+            this.edit.TabIndex = 3;
+            this.edit.Text = "Edytuj";
+            this.edit.UseVisualStyleBackColor = true;
+            // 
+            // discard
+            // 
+            this.discard.Location = new System.Drawing.Point(494, 208);
+            this.discard.Name = "discard";
+            this.discard.Size = new System.Drawing.Size(75, 33);
+            this.discard.TabIndex = 4;
+            this.discard.Text = "Anuluj";
+            this.discard.UseVisualStyleBackColor = true;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(578, 208);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 33);
+            this.save.TabIndex = 5;
+            this.save.Text = "Zapisz";
+            this.save.UseVisualStyleBackColor = true;
             // 
             // WynikiForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(665, 488);
+            this.ClientSize = new System.Drawing.Size(665, 400);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.discard);
+            this.Controls.Add(this.edit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "WynikiForm";
             this.Text = "WynikiForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.WynikiForm_FormClosed);
             this.Load += new System.EventHandler(this.WynikiForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.aMTSDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.wynikBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -88,5 +145,10 @@
         private System.Windows.Forms.BindingSource wynikBindingSource;
         private AMTSDataSetTableAdapters.wynikTableAdapter wynikTableAdapter;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button edit;
+        private System.Windows.Forms.Button discard;
+        private System.Windows.Forms.Button save;
     }
 }
