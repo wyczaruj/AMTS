fa67168b15d2b5c17bf887103d9276049ac8c23c
patrykvan <patrykvan@gmail.com>
2016-06-04 22:38:58 +0200

    Dodany przycisk generowania raportu(jeszcze nie ułożony w odpowiednim miejscu)

--- AMTS/Properties/Resources.Designer.cs
+++ AMTS/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace AMTS.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap generuj {
+            get {
+                object obj = ResourceManager.GetObject("generuj", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap googleplus {
             get {
                 object obj = ResourceManager.GetObject("googleplus", resourceCulture);
